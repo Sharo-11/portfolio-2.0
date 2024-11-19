9147774158444b8d2c29ec7c9ef7f24b131b5873
Sharo11 <sharodubey1312@gmail.com>
2024-11-19 14:55:19 +0530

    Made Responsive

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/Navbar.css';
 
 const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className='logo'>
-      <a href="#hero" className="name">Sharvari.</a>
+        <a href="#hero" className="name">Sharvari.</a>
       </div>
-      <div className='nav-links'>
-       <a href="#hero" className="nav-item">Home</a>
-       <a href="#learnings" className="nav-item">Learnings</a>
-       <a href="#projects" className="nav-item">Projects</a>
-       <a href="#reports" className="nav-item">Reports</a>
-       <a href="#achievements" className="nav-item">Achievements</a>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
-
+      <div className={`nav-links ${menuActive ? 'active' : ''}`}>
+        <a href="#hero" className="nav-item">Home</a>
+        <a href="#learnings" className="nav-item">Learnings</a>
+        <a href="#projects" className="nav-item">Projects</a>
+        <a href="#reports" className="nav-item">Reports</a>
+        <a href="#achievements" className="nav-item">Achievements</a>
+      </div>
     </nav>
   );
 };
