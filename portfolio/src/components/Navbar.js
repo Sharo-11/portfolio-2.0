6e58b6e9d663740f77af832fc821a401747d5e86
Sharo Dubey <sharodubey1312@gmail.com>
2025-01-20 14:53:19 +0000

    responsive

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,6 +4,7 @@ import './styles/Navbar.css';
 const Navbar = () => {
   const [menuActive, setMenuActive] = useState(false);
 
+  // Toggle menu state on hamburger click
   const toggleMenu = () => {
     setMenuActive(!menuActive);
   };
@@ -13,11 +14,14 @@ const Navbar = () => {
       <div className='logo'>
         <a href="#hero" className="name">Sharvari.</a>
       </div>
+      {/* Hamburger Menu for mobile screens */}
       <div className="hamburger-menu" onClick={toggleMenu}>
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
+
+      {/* Nav links with active class based on menu state */}
       <div className={`nav-links ${menuActive ? 'active' : ''}`}>
         <a href="#hero" className="nav-item">Home</a>
         <a href="#learnings" className="nav-item">Learnings</a>
